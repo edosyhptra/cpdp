7c5a8611bc304a06ebe89b5511753b27a6e9fb86
edosyhptra <edo23syahputra@gmail.com>
2025-03-10 10:41:23 +0700

    fix secret key in env

--- src/cpdp/settings.py
+++ src/cpdp/settings.py
@@ -39,7 +39,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gl8&c96lgkdr6b$n)w1mq*47z=78+q5%ciqd#jg(k9i29*8g*u'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
